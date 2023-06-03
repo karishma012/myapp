f1d44fef76d57bd5971bac4f7dbdd4f151cccfb7
karishma012 <115379019+karishma012@users.noreply.github.com>
2023-06-04 00:03:54 +0530

    till 56

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 
+import { Routes, Route } from 'react-router-dom';
 
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <>
+      <h1>This is me</h1>
+      <Navbar/>
+      <Routes>
+        <Route path="/Home" element={< Home />} />
+
+        <Route path="/About" element={< About />} />
+
+      </Routes>
     </>
   );
 }
